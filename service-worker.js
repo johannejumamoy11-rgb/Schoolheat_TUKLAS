c8d3c995ec39f80a8acb42d43228047d8f00f4e6
johannejumamoy11-rgb <johannejumamoy11@gmail.com>
2026-08-28 23:05:15 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sh-v31';
+const CACHE = 'sh-v32';
 const FILES = ['./','./index.html','./style.css','./script.js','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
